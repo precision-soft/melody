39dfe8caa4116dc945462f5f915559c153619df5
Adrian Jeledintan <adrian.jeledintan@gmail.com>
2026-02-03 09:19:48 +0200

    exception: include causeChain in LogContext

--- exception/utility.go
+++ exception/utility.go
@@ -37,6 +37,37 @@ func LogContext(err error, extra ...exceptioncontract.Context) exceptioncontract
 		}
 	}
 
+	var exceptionValue *Error
+	if true == errors.As(err, &exceptionValue) && nil != exceptionValue {
+		causeErr := exceptionValue.CauseErr()
+		if nil != causeErr {
+			_, hasCause := context["cause"]
+			_, hasCauseChain := context["causeChain"]
+
+			if false == hasCause || false == hasCauseChain {
+				causeChain := buildCauseChain(causeErr, 8)
+				if 0 < len(causeChain) {
+					if false == hasCause {
+						context["cause"] = causeChain[0]
+					}
+					if false == hasCauseChain {
+						context["causeChain"] = causeChain
+					}
+				} else if false == hasCause {
+					context["cause"] = causeErr.Error()
+				}
+			}
+
+			_, hasCauseContextChain := context["causeContextChain"]
+			if false == hasCauseContextChain {
+				causeContextChain := buildCauseContextChain(causeErr, 8)
+				if 0 < len(causeContextChain) {
+					context["causeContextChain"] = causeContextChain
+				}
+			}
+		}
+	}
+
 	if 0 == len(extra) || nil == extra[0] {
 		return context
 	}
@@ -108,4 +139,61 @@ func copyStringMap[T any](input map[string]T) map[string]T {
 	}
 
 	return copied
+}
+
+func buildCauseChain(causeErr error, maxDepth int) []string {
+	if nil == causeErr {
+		return nil
+	}
+
+	if 0 >= maxDepth {
+		return []string{causeErr.Error()}
+	}
+
+	chain := make([]string, 0, maxDepth)
+
+	current := causeErr
+	for depth := 0; depth < maxDepth && nil != current; depth++ {
+		chain = append(chain, current.Error())
+		current = errors.Unwrap(current)
+	}
+
+	return chain
+}
+
+func buildCauseContextChain(causeErr error, maxDepth int) []map[string]any {
+	if nil == causeErr {
+		return nil
+	}
+
+	if 0 >= maxDepth {
+		maxDepth = 1
+	}
+
+	chain := make([]map[string]any, 0, maxDepth)
+	hasAnyContext := false
+
+	current := causeErr
+	for depth := 0; depth < maxDepth && nil != current; depth++ {
+		var causeException *Error
+		if true == errors.As(current, &causeException) {
+			causeContext := causeException.Context()
+			if nil != causeContext && 0 < len(causeContext) {
+				chain = append(chain, causeContext)
+				hasAnyContext = true
+			} else {
+				chain = append(chain, nil)
+			}
+		} else {
+			chain = append(chain, nil)
+		}
+
+		current = errors.Unwrap(current)
+	}
+
+	if false == hasAnyContext {
+		return nil
+	}
+
+	return chain
 }
